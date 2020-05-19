5382c123185222e699aeeb7cdc5c3fd859a8d6ff
Yury Bolbukou <32056409+Nikan888@users.noreply.github.com>
2020-05-20 00:05:42 +0300

    task3

--- src/App.js
+++ src/App.js
@@ -6,36 +6,106 @@ import CardContent from "./Card/CardContent";
 import CardHeader from "./Card/CardHeader";
 import Checkbox from "./Card/Checkbox";
 import { DEFAULT, CHECKED } from "./Card/variant";
+import { MdSave, MdCancel, MdEdit } from "react-icons/md";
 
 class App extends Component {
   state = {
     checked: false,
-    variant: DEFAULT,
+    isEdit: false,
+    cardHeaderText: "Caption",
+    cardContentText: "Text",
+    cardHeaderTextBuff: "",
+    cardContentTextBuff: "",
   };
 
-  handleCheckboxChange = (event) => {
+  handleCheckboxChange = () => {
     this.setState({
-      checked: event.target.checked,
-      variant: event.target.checked ? CHECKED : DEFAULT,
+      checked: !this.state.checked,
     });
   };
 
+  editHandler = () => {
+    this.setState({
+      checked: false,
+      isEdit: !this.state.isEdit,
+      cardHeaderTextBuff: this.state.cardHeaderText,
+      cardContentTextBuff: this.state.cardContentText,
+    });
+  };
+
+  changeCardHeaderHandler = (event) => {
+    this.setState({ cardHeaderTextBuff: event.target.value });
+  };
+
+  changeCardContentHandler = (event) => {
+    this.setState({ cardContentTextBuff: event.target.value });
+  };
+
+  saveHandler = () => {
+    this.setState({
+      cardHeaderText: this.state.cardHeaderTextBuff,
+      cardContentText: this.state.cardContentTextBuff,
+      isEdit: !this.state.isEdit,
+    });
+  };
+
+  cancelHandler = () => {
+    this.setState({ isEdit: !this.state.isEdit });
+  };
+
   render() {
+    let actions;
+    let header;
+    let content;
+    let cardVariant = this.state.checked ? CHECKED : DEFAULT;
+
+    if (this.state.isEdit) {
+      actions = (
+        <div className="actions-position">
+          <MdSave onClick={this.saveHandler} />
+          <MdCancel onClick={this.cancelHandler} />
+        </div>
+      );
+      header = (
+        <div>
+          <input
+            type="text"
+            value={this.state.cardHeaderTextBuff}
+            onChange={this.changeCardHeaderHandler}
+          />
+        </div>
+      );
+      content = (
+        <div>
+          <input
+            type="text"
+            value={this.state.cardContentTextBuff}
+            onChange={this.changeCardContentHandler}
+          />
+        </div>
+      );
+    } else {
+      actions = (
+        <div className="actions-position">
+          <MdEdit onClick={this.editHandler} />
+          <Checkbox
+            checked={this.state.checked}
+            onChange={this.handleCheckboxChange}
+          />
+        </div>
+      );
+      header = <CardHeader text={this.state.cardHeaderText} />;
+      content = <CardContent text={this.state.cardContentText} />;
+    }
+
     return (
       <div className="App">
         <Header />
         <div className="wrapper">
-          <Card variant={this.state.variant}>
-            <CardHeader
-              checkbox={
-                <Checkbox
-                  checked={this.state.checked}
-                  onChange={this.handleCheckboxChange}
-                />
-              }
-              text={"Caption"}
-            ></CardHeader>
-            <CardContent text={"Text"} />
+          <Card variant={cardVariant}>
+            {actions}
+            {header}
+            {content}
           </Card>
         </div>
       </div>
--- src/Card/CardHeader.js
+++ src/Card/CardHeader.js
@@ -2,12 +2,7 @@ import React from "react";
 import "./CardHeader.css";
 
 const CardHeader = (props) => {
-  return (
-    <div className="Card-Header">
-      <div className="checkbox-position">{props.checkbox}</div>
-      {props.text}
-    </div>
-  );
+  return <div className="Card-Header">{props.text}</div>;
 };
 
 export default CardHeader;
