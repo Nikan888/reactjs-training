64a85ea383223ba01b2da33a65626b966cc22b27
Yury Bolbukou <32056409+Nikan888@users.noreply.github.com>
2020-07-22 11:59:59 +0300

    task 14 fix 1

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -2,42 +2,27 @@ import React from "react";
 import "./MainHeader.css";
 import { Link, withRouter } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import { initializeCards } from "../../store/actions";
-import { v4 as uuidv4 } from "uuid";
+import { loadCards } from "../../store/actions";
 
 const MainHeader = () => {
   const cards = useSelector((state) => state.cards);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios
-      .get(
-        "https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json"
-      )
-      .then((response) => {
-        dispatch(
-          initializeCards(
-            response.data.splice(0, 15).map((row) => ({
-              id: uuidv4(),
-              headerText: row.Name,
-              bodyText: row.About,
-            }))
-          )
-        );
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    dispatch(loadCards());
   }, []);
 
   return (
     <header>
       <button className="link-button">
-        <Link to="/" className="badge">Home</Link>
+        <Link to="/" className="badge">
+          Home
+        </Link>
       </button>
       <button className="link-button">
-        <Link to="/auth" className="badge">Sign in</Link>
+        <Link to="/auth" className="badge">
+          Sign in
+        </Link>
       </button>
       <button className="cardsCounter">
         Cards count: <span className="badge">{cards.length}</span>
--- src/store/actions.js
+++ src/store/actions.js
@@ -4,6 +4,8 @@ import {
   UPDATE_CARD,
   REMOVE_CARDS,
 } from "./actionsTypes";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 export const initializeCards = (cards) => ({
   cards,
@@ -28,3 +30,24 @@ export const removeCards = (cardsRecycleBin) => ({
   cardsRecycleBin,
   type: REMOVE_CARDS,
 });
+
+export const loadCards = () => (dispatch) => {
+  axios
+    .get(
+      "https://raw.githubusercontent.com/BrunnerLivio/PokemonDataGraber/master/output.json"
+    )
+    .then((response) => {
+      dispatch(
+        initializeCards(
+          response.data.splice(0, 15).map((row) => ({
+            id: uuidv4(),
+            headerText: row.Name,
+            bodyText: row.About,
+          }))
+        )
+      );
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
